4abdfaed4c1cc81b49f6c5b63c3cfda164c64ebd
David Luecke <daff@neyeon.com>
2013-03-03 11:17:18 -0500

    TodoMVC FuncUnit tests

---
+++ test/spec.js
@@ -0,0 +1,35 @@
+localStorage.clear();
+
+module("TodoMVC spec", {
+	setup: function() {
+		S.open('../architecture-examples/canjs/index.html');
+	}
+});
+
+test("Creates a new Todo", function() {
+	S('#new-todo').click().type('Do some nerdy stuff\r').wait(500);
+	S('#todo-list li').size(1, 'Got one Todo');
+	S('#todo-list li:first label').html('Do some nerdy stuff', 'Todo has correct text');
+	S('#todo-count').html(/<strong>1<\/strong>(.*)item(.*)left/, 'Todo count text is correct');
+});
+
+test("Marks a Todo as complete", function() {
+	S('#todo-list li:first .toggle').click().wait(500);
+	S('#todo-list li:first').hasClass('completed', true, 'Todo is completed');
+	S('#clear-completed').html(/Clear completed \(1\)/, 'Completed button HTML is correct');
+	S('#todo-count').html(/<strong>0<\/strong>(.*)item(.*)left/, 'Todo count text is correct')
+});
+
+test("Adding another Todo, filtering", function() {
+	S('#new-todo').click().type('Finish website\r').wait(500);
+	S('#todo-list li').size(2, 'Now two Todos in list');
+	S('#todo-count').html(/<strong>1<\/strong>(.*)item(.*)left/, 'Todo count text is correct');
+});
+
+//test("Deleting a Todo", function() {
+//
+//});
+//
+//test("Complete and clear all", function() {
+//
+//});
